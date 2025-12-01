81a421f94f138ec102a35a89128627b5c327f0c6
mindcrackx <mindcrackx@protonmail.com>
2025-12-01 23:27:57 +0100

    feat(day1): add day 1

--- days/01.go
+++ days/01.go
@@ -1,14 +1,79 @@
 package days
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
 	"io"
+
+	"github.com/mindcrackx/aoc2025/utils"
 )
 
 func One_1(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+
+	rotation := 50
+	countZeroRotation := 0
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		rotaDirection := line[0]
+		rotaNum := utils.MustAtoi(line[1:])
+
+		switch rotaDirection {
+		case 'L':
+			rotation = (rotation - rotaNum + 100) % 100
+		case 'R':
+			rotation = (rotation + rotaNum) % 100
+		}
+
+		// slog.Info("", "rotation", rotation, "line", line, "rotationMod", (rotation+100)%100, "countZeroRotation", countZeroRotation)
+
+		if rotation == 0 {
+			countZeroRotation++
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d", countZeroRotation), nil
 }
 
 func One_2(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+
+	rotation := 50
+	countZeroRotation := 0
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		rotaDirection := line[0]
+		rotaNum := utils.MustAtoi(line[1:])
+
+		switch rotaDirection {
+		case 'L':
+			for i := 0; i < rotaNum; i++ {
+				rotation--
+				rotation = (rotation + 100) % 100
+				if rotation == 0 {
+					countZeroRotation++
+				}
+			}
+		case 'R':
+			for i := 0; i < rotaNum; i++ {
+				rotation++
+				rotation = (rotation + 100) % 100
+				if rotation == 0 {
+					countZeroRotation++
+				}
+			}
+		}
+
+		// slog.Info("", "countZeroRotation", countZeroRotation, "line", line, "rotation", rotation)
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d", countZeroRotation), nil
 }
--- days/01_test.go
+++ days/01_test.go
@@ -14,7 +14,22 @@ func TestOne_1(t *testing.T) {
 		name   string
 		input  string
 		expect string
-	}{}
+	}{
+		{
+			name: "example 1",
+			input: `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`,
+			expect: "3",
+		},
+	}
 
 	for _, tt := range tests {
 		tt := tt
@@ -40,7 +55,22 @@ func TestOne_2(t *testing.T) {
 		name   string
 		input  string
 		expect string
-	}{}
+	}{
+		{
+			name: "example 1",
+			input: `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`,
+			expect: "6",
+		},
+	}
 
 	for _, tt := range tests {
 		tt := tt
