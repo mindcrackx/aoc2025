5b23f44d88027ef4f78c288f1b132108b4b6acd6
mindcrackx <mindcrackx@protonmail.com>
2025-12-02 08:57:25 +0100

    feat(day02): initial day2

--- days/02.go
+++ days/02.go
@@ -1,14 +1,68 @@
 package days
 
 import (
-	"errors"
+	"fmt"
 	"io"
+	"strings"
+
+	"github.com/mindcrackx/aoc2025/utils"
 )
 
 func Two_1(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+	data, err := io.ReadAll(input)
+	if err != nil {
+		return "", err
+	}
+
+	result := 0
+
+	parts := strings.Split(strings.TrimSpace(string(data)), ",")
+	for _, part := range parts {
+		ids := strings.Split(part, "-")
+		for i := utils.MustAtoi(ids[0]); i <= utils.MustAtoi(ids[1]); i++ {
+			istr := fmt.Sprintf("%d", i)
+			if len(istr)%2 != 0 {
+				continue
+			}
+
+			if istr[0:len(istr)/2] == istr[len(istr)/2:] {
+				// slog.Info("found", "istr", istr)
+				result += i
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d", result), nil
 }
 
 func Two_2(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+	data, err := io.ReadAll(input)
+	if err != nil {
+		return "", err
+	}
+
+	result := 0
+
+	parts := strings.Split(strings.TrimSpace(string(data)), ",")
+	for _, part := range parts {
+		ids := strings.Split(part, "-")
+		for i := utils.MustAtoi(ids[0]); i <= utils.MustAtoi(ids[1]); i++ {
+			istr := fmt.Sprintf("%d", i)
+
+			for x := range len(istr) {
+				if x == 0 {
+					continue
+				}
+				repeat := strings.Repeat(istr[0:x], len(istr)/(x))
+				if istr == repeat {
+					// slog.Info("found", "istr", istr)
+					result += i
+					break
+				}
+			}
+
+		}
+	}
+
+	return fmt.Sprintf("%d", result), nil
 }
--- days/02_test.go
+++ days/02_test.go
@@ -14,7 +14,18 @@ func TestTwo_1(t *testing.T) {
 		name   string
 		input  string
 		expect string
-	}{}
+	}{
+		{
+			name:   "example 0",
+			input:  "11-22",
+			expect: "33",
+		},
+		{
+			name:   "example 1",
+			input:  "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+			expect: "1227775554",
+		},
+	}
 
 	for _, tt := range tests {
 		tt := tt
@@ -40,7 +51,18 @@ func TestTwo_2(t *testing.T) {
 		name   string
 		input  string
 		expect string
-	}{}
+	}{
+		{
+			name:   "example 0",
+			input:  "95-115",
+			expect: "210",
+		},
+		{
+			name:   "example 1",
+			input:  "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+			expect: "4174379265",
+		},
+	}
 
 	for _, tt := range tests {
 		tt := tt
