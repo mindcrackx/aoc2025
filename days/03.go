3082265daafd89d8552f1fdc540decf76630527c
mindcrackx <mindcrackx@protonmail.com>
2025-12-03 14:36:50 +0100

    feat(day03): initial solution

--- days/03.go
+++ days/03.go
@@ -1,14 +1,96 @@
 package days
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
 	"io"
+
+	"github.com/mindcrackx/aoc2025/utils"
 )
 
 func Three_1(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+	var resultTotal int
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		nums := make([]int, 0, len(line))
+		for _, r := range line {
+			nums = append(nums, utils.MustAtoi(string(r)))
+		}
+
+		first := -1
+		second := -1
+		for i := 0; i < len(nums)-1; i++ {
+			if nums[i] <= first {
+				continue
+			}
+			first = nums[i]
+			tmpSecond := -1
+
+			for y := i + 1; y < len(nums); y++ {
+				if nums[y] <= tmpSecond {
+					continue
+				}
+				tmpSecond = nums[y]
+			}
+			second = tmpSecond
+		}
+		resultTotal += utils.MustAtoi(fmt.Sprintf("%d%d", first, second))
+		// slog.Info("", "resultTotal", resultTotal, "first", first, "second", second)
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d", resultTotal), nil
 }
 
 func Three_2(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+	var resultTotal int
+
+	nextBest := func(nums []int, currentI, remaining int) (int, int) {
+		currBest := -1
+		currI := -1
+		for i := currentI + 1; i < len(nums)-remaining+1; i++ {
+			if nums[i] <= currBest {
+				continue
+			}
+			currBest = nums[i]
+			currI = i
+		}
+		return currI, currBest
+	}
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		nums := make([]int, 0, len(line))
+		for _, r := range line {
+			nums = append(nums, utils.MustAtoi(string(r)))
+		}
+
+		bestNums := make([]int, 0, 12)
+		var currBest = -1
+		var currI = -1
+
+		for i := range 12 {
+			currI, currBest = nextBest(nums, currI, 12-i)
+			bestNums = append(bestNums, currBest)
+			// slog.Info("", "i", i, "currI", currI, "currBest", currBest, "bestNums", bestNums)
+		}
+
+		bestStr := ""
+		for _, n := range bestNums {
+			bestStr += fmt.Sprintf("%d", n)
+		}
+		resultTotal += utils.MustAtoi(bestStr)
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d", resultTotal), nil
 }
--- days/03_test.go
+++ days/03_test.go
@@ -14,7 +14,36 @@ func TestThree_1(t *testing.T) {
 		name   string
 		input  string
 		expect string
-	}{}
+	}{
+		{
+			name: "example 1",
+			input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+			expect: "357",
+		},
+		{
+			name:   "example 2",
+			input:  "987654321111111",
+			expect: "98",
+		},
+		{
+			name:   "example 3",
+			input:  "811111111111119",
+			expect: "89",
+		},
+		{
+			name:   "example 4",
+			input:  "234234234234278",
+			expect: "78",
+		},
+		{
+			name:   "example 5",
+			input:  "818181911112111",
+			expect: "92",
+		},
+	}
 
 	for _, tt := range tests {
 		tt := tt
@@ -40,7 +69,36 @@ func TestThree_2(t *testing.T) {
 		name   string
 		input  string
 		expect string
-	}{}
+	}{
+		{
+			name: "example 1",
+			input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+			expect: "3121910778619",
+		},
+		{
+			name:   "example 2",
+			input:  "987654321111111",
+			expect: "987654321111",
+		},
+		{
+			name:   "example 3",
+			input:  "811111111111119",
+			expect: "811111111119",
+		},
+		{
+			name:   "example 4",
+			input:  "234234234234278",
+			expect: "434234234278",
+		},
+		{
+			name:   "example 5",
+			input:  "818181911112111",
+			expect: "888911112111",
+		},
+	}
 
 	for _, tt := range tests {
 		tt := tt
