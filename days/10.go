5daec602f4bdb099abb79df7e851ce7862ffeab3
mindcrackx <marcel.plaumann1@protonmail.com>
2025-12-14 21:59:19 +0100

    todo(day10): part 2 in progress

--- days/10.go
+++ days/10.go
@@ -2,9 +2,10 @@ package days
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
+	"math"
+	"slices"
 	"strings"
 
 	"github.com/mindcrackx/aoc2025/utils"
@@ -94,5 +95,126 @@ func tenSolveBFS(target []int, buttons [][]int) []int {
 }
 
 func Ten_2(input io.Reader) (string, error) {
-	return "", errors.New("not implemented")
+	var resultTotal int
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		idx1 := strings.Index(line, "]")
+		idx2 := strings.Index(line, "{")
+
+		// lightsStr := strings.TrimSpace(line[0 : idx1+1])
+		buttonsStr := strings.TrimSpace(line[idx1+1 : idx2-1])
+		joltageStr := strings.TrimSpace(line[idx2+1 : len(line)-1])
+
+		buttons := make([][]int, 0)
+		for i, bStr := range strings.Fields(buttonsStr) {
+			buttonsStrs := strings.Split(bStr[1:len(bStr)-1], ",")
+			buttons = append(buttons, make([]int, len(buttonsStrs)))
+			for ii, b := range buttonsStrs {
+				buttons[i][ii] = utils.MustAtoi(b)
+			}
+		}
+
+		joltages := make([]int, 0)
+		for _, j := range strings.Split(joltageStr, ",") {
+			joltages = append(joltages, utils.MustAtoi(j))
+		}
+
+		fmt.Println("buttons: ", buttons)
+		fmt.Println("joltages:", joltages)
+		result := solveTenPart2(joltages, buttons)
+		resultTotal += result
+		fmt.Printf(" |->  %d\n", result)
+		fmt.Println(strings.Repeat("=", 80))
+
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d", resultTotal), nil
+}
+
+func solveTenPart2(target []int, buttons [][]int) int {
+	best := math.MaxInt
+
+	lastBtnIdxForCounter := make([]int, len(target))
+	for i := range lastBtnIdxForCounter {
+		lastBtnIdxForCounter[i] = -1
+	}
+	for btnIdx, btn := range buttons {
+		for _, counterIdx := range btn {
+			lastBtnIdxForCounter[counterIdx] = btnIdx
+		}
+	}
+
+	var solve func(btnIdx int, current []int, pressesTotal int)
+	solve = func(btnIdx int, current []int, pressesTotal int) {
+		if pressesTotal >= best {
+			return // we already found a better solution
+		}
+
+		// pruning 1
+		for i := range current {
+			if lastBtnIdxForCounter[i] < btnIdx {
+				// no remaining btn affects counter i, impossible to reach target
+				if current[i] != target[i] {
+					return
+				}
+			}
+		}
+
+		// pruning 2
+		// estimate min presses still needed
+		minRemaining := 0
+		for i := range current {
+			diff := target[i] - current[i]
+			if diff > minRemaining {
+				minRemaining = diff
+			}
+		}
+		// each btn press can only increase by 1!
+		if pressesTotal+minRemaining >= best {
+			return
+		}
+
+		if btnIdx == len(buttons) {
+			if slices.Equal(current, target) {
+				if pressesTotal < best {
+					best = pressesTotal
+				}
+			}
+			return
+		}
+
+		var exceeded bool
+		for presses := 0; ; presses++ {
+			exceeded = false
+			for i := range current {
+				if current[i] > target[i] {
+					exceeded = true
+					break
+				}
+			}
+			if exceeded {
+				break
+			}
+
+			next := make([]int, len(current))
+			copy(next, current)
+
+			// recurse
+			solve(btnIdx+1, next, pressesTotal+presses)
+
+			// press btn again for next iteration
+			for _, idx := range buttons[btnIdx] {
+				current[idx]++
+			}
+		}
+	}
+
+	solve(0, make([]int, len(target)), 0)
+	return best
 }
